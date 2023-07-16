2d0f853008b358197ebab8ab89c4889b617b5b90
José Carlos <45600461+DevJoseCarlosLSA@users.noreply.github.com>
2023-07-16 13:44:42 -0300

    Curso JavaScript #05

---
+++ Arquivos/4_variaveis/js/scripts.js
@@ -0,0 +1,47 @@
+var teste = 1;
+
+//var Nome = "João" >> ERRADO
+var nome = "João";
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(nome);
+
+console.log(teste);
+
+//var &nome = 'ASD'; >>ERRADO
+var $nome = 'ASD';
+
+//var -nome = 'ASD2';>>ERRADO
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+//var 5nome = 'ASD3'; >>ERRADO
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Matheus';
+
+console.log(meuPrimeiroNome); //camelCase
+
+var meusobrenome = "Battisti";
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+meuNome = "Matheus";
+
+console.log(meuNome);
+                   
