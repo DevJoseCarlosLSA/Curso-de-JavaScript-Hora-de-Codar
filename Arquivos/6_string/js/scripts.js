3db95c6c77db5a6e310d7fe51d1fe6ccb6ab152e
José Carlos <45600461+DevJoseCarlosLSA@users.noreply.github.com>
2023-07-17 02:54:19 -0300

    Curso JavaScript #07

---
+++ Arquivos/6_string/js/scripts.js
@@ -0,0 +1,20 @@
+var nome = "Matheus";
+var sobrenome = "Battisti";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = 'Esta é uma frase  complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
